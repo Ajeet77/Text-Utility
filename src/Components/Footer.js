c751c98089aed59ddc8e450b99df223ef0b573ab
Ajeet Kumar <kumar.ajeet889@gmail.com>
2023-02-15 23:14:47 +0530

    initial commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../Styles/Footer.css";
+export default function Footer() {
+  return (
+    <>
+      <div className="footerContainer">
+        <footer>
+          <p>React Js ©2027.All right reserved</p>
+        </footer>
+      </div>
+    </>
+  );
+}
